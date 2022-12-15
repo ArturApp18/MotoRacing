6165e534ac13112c4a041145f837ab8e33fa92ca
ArturApp18 <artur.app19@gmail.com>
2022-12-15 20:19:44 +0100

    update Physics

--- MotoSpeedPreview/Assets/Game/Scripts/WheelMove.cs
+++ MotoSpeedPreview/Assets/Game/Scripts/WheelMove.cs
@@ -1,27 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class WheelMove : MonoBehaviour
+namespace Game.Scripts
 {
-    [SerializeField] private float speed;
+    public class WheelMove : MonoBehaviour
+    {
+        [SerializeField] private float speed;
     
-    private WheelJoint2D _wheel;
-    private JointMotor2D _motor;
+        private WheelJoint2D _wheel;
+        private JointMotor2D _motor;
 
-    private void Start()
-    {
-        _wheel = GetComponentInParent<WheelJoint2D>();
-        _motor = _wheel.motor;
-    }
+        private void Start()
+        {
+            _wheel = GetComponentInParent<WheelJoint2D>();
+            _motor = _wheel.motor;
+        }
+        
+        private void Update()
+        {
+            float hForce = Input.GetAxis("Horizontal");
 
-    // Update is called once per frame
-    private void Update()
-    {
-        float hForce = Input.GetAxis("Horizontal");
+            _motor.motorSpeed = speed * hForce;
 
-        _motor.motorSpeed = speed * hForce;
-
-        _wheel.motor = _motor;
+            _wheel.motor = _motor;
+        }
     }
 }
